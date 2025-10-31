552599b5d5eb9e5aa96527e880c04bc8d3d93e6b
Daniel War <daniel.w93f@gmail.com>
2025-10-31 16:36:13 -0600

    creando tablas

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ class Estudiante(db.Model):
             'ap_materno': self.ap_materno,
             'semestre': self.semestre,
         }
+    
+    with app.app_context():
+        db.create_all()
 
 
 #Ruta raiz
